9414f4931fbbe20ff66e21d7c4bd0eb36560dde8
Isaac Cohen <isaaclandoncohen@gmail.com>
2014-12-11 11:51:49 -0800

    adding some demo choreography

--- Choreography.js
+++ Choreography.js
@@ -16,9 +16,37 @@ Choreography.prototype.addTweenEvent = function(){
 
 }
 
-Choreography.prototype.addCameraPositionEvent = function(){
+Choreography.prototype.tweenCamera = function( p1 , p2 , t1 , t2 ){
+
+  var i = { x: p1[0] , y:p1[1] , z:p1[2] }
+  var f = { x: p2[0] , y:p2[1] , z:p2[2] }
+
+  var t = new TWEEN.Tween( i ).to( f , (t2 - t1 ) * 1000 );
+
+  t.onUpdate( function(){
+    camera.position.copy( i );
+  });
+
+  var cb = function(){
+    
+    camera.position.copy( i );
+    t.start();
+
+  }
+
+  this.addEvent( cb , t1 );
 
 
+}
+
+Choreography.prototype.positionCamera = function( p , t ){
+
+
+  var cb = function(){
+    camera.position.set( p[0] , p[1] , p[2] );
+  }
+
+  this.addEvent( cb , t );
 
 }
 
--- initChoreography.js
+++ initChoreography.js
@@ -3,22 +3,33 @@ function initChoreography(){
 
   choreography = new Choreography();
 
+  var c = choreography;
 
-  choreography.addEvent( function(){
+  c.addEvent( function(){
 
     snowflakes[0].activate();
-
 
   // First number has to be slightly above 0
   // because I don't know how to program
   } , 0.0001);
 
+  c.tweenCamera( 
+    [ -1 , 0 , 1 ] ,  // Start Position
+    [ 1 , 0 , 1 ] ,   // End Position
+    1,                // Start time ( seconds ) 
+    5                 // End Time ( seconds )
+  );
+
+  c.positionCamera(
+    [ 0 , 0 , 1 ],    // Position
+    5                 // Time ( seconds )
+  );
 
   choreography.addEvent( function(){
-
+  
     var p = new THREE.Vector3( 2 , 0 , -1 );
     snowflakes[1].activate(p);
-
+  
   } , 3 );
 
   
@@ -30,6 +41,8 @@ function initChoreography(){
     snowflakes[2].activate(p);
 
   } , 4 );
+
+  //choreography.positionCamera( [ -1 , 0 , 1 ] , 4 );
 
 
 
