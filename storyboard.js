6f265a8f60fd1ac89cf0a0195331236d25df077c
Isaac Cohen <isaaclandoncohen@gmail.com>
2014-12-22 11:36:50 -0800

    Beginning work of brining in Post Processing. Not working very well so far. BROKEN COMMIT

---
+++ storyboard.js
@@ -0,0 +1,273 @@
+var flakeStoryline = parseStoryline( {
+
+	flake1a: [
+		'0 cut to .3433',
+		'30 ease to -5.123'
+	],
+
+	flake2a: [
+		'27 cut to 1.3433',
+		'40 ease to 3.123'
+	],
+
+	flake3a: [
+		'35 cut to 2.3433',
+		'60 ease to 4.123'
+	],
+
+	flake4a: [
+		'50 cut to 2.3433',
+		'65 ease to -1.123',
+		'66 cut to -1.123',
+		'80 ease to 2.123'
+	],
+	
+	flake5a: [
+		'70 cut to .3433',
+		'107 ease to -5.123'
+	]
+
+} );
+
+var effectsStoryline = parseStoryline( {
+
+	fade: [
+		'0 cut to 1',
+		'11 ease to 1',
+		'14 ease to 0',
+
+		'106 ease to 0',
+		'107 ease to 1'
+	]
+
+} );
+
+var cameraStoryline = parseStoryline( {
+	
+	x: [
+		'0 cut to 0',
+		'97 ease to 0'
+	],
+
+	y: [
+		'0 cut to -1400',
+		'11 cut to -800',
+		'97 linear to 750',
+		'107 linear to 900'
+	],
+
+	z: [
+		'0 cut to 0',
+		'97 ease to 0'
+	],
+	
+	targetx: [
+		'0 cut to 20',
+		'97 ease to 20',
+	],
+
+	targety: [
+		'0 cut to -820',
+		'11 cut to -820',
+		'97 linear to 800',
+		'107 linear to 1000'
+	],
+
+	targetz: [
+		'0 cut to 50',
+		'97 ease to 50',
+	],
+	
+	cutx: [
+		'17 cut to -10',
+		'19.5 ease to 10',
+		'23 cut to 0',
+		'24 ease to 5',
+		'24 cut to -15',
+		'26.5 linear to 15',
+		'30 cut to 0',
+		'35 ease to 0',
+
+		'42 cut to -10',
+		'44.5 ease to -10',
+		'44.5 cut to -10',
+		'48 ease to -10',
+		
+		'49.3 cut to 0',
+		'51 ease to 0',
+
+		'54 cut to -10',
+		'66 ease to 10',
+
+		'69.5 cut to -50',
+		'75 ease to -60',
+
+		'75 cut to 0',
+		'90 ease to 0',
+
+		'90 cut to 0',
+		'107 ease to 0',
+	],
+
+	cuty: [
+		'17 cut to -750',
+		'19.5 ease to -720',
+		'23 cut to -750',
+		'24 ease to -750',
+		'24 cut to -750',
+		'26.5 ease to -750',
+		'27 cut to -500',
+		'30 ease to -375',
+		'30 cut to -375',
+		'35 ease to -375',
+
+		'42 cut to 0',
+		'44.5 ease to 10',
+		'44.5 cut to -20',
+		'48 linear to 20',
+
+		'49.3 cut to 0',
+		'51 ease to 0',
+
+		'54 cut to 375',
+		'66 ease to 375',
+
+		'69.5 cut to 355',
+		'75 linear to 395',
+
+		'75 cut to 750',
+		'90 ease to 750',
+
+		'90 cut to 750',
+		'107 ease to 1000',
+	],
+
+	cutz: [
+		'17 cut to -50',
+		'18 ease to -60',
+		'19.5 ease to -50',
+		'24 cut to 50',
+		'26.5 linear to 50',
+		'30 cut to 0',
+		'30.5 ease to 20',
+		'32.5 cut to -80',
+		'35 ease to -100',
+
+		'42 cut to -10',
+		'44.5 ease to -80',
+		'44.5 cut to 40',
+		'48 ease to 80',
+
+		'49.3 cut to 0',
+		'51 ease to 0',
+
+		'54 cut to 80',
+		'60 ease to 80',
+		'62 cut to 20',
+		'66 ease to 0',
+		'66 cut to 40',
+		'66 ease to -20',
+
+		'69.5 cut to 40',
+		'75 ease to 50',
+
+		'75 cut to 100',
+		'87 linear to 10',
+		'87 cut to 0',
+		'89 ease to 0',
+		'89 cut to -50',
+		'90 ease to -50',
+		
+		'107 ease to 0',
+	],
+
+	targetcutx: [
+		'23 cut to 0',
+		'24 ease to 5',
+
+		'24 cut to -10',
+		'26.5 linear to 10',
+		'30 cut to 0',
+		'35 ease to 0',
+
+		'42 cut to 0',
+		'44.5 ease to 0',
+		'44.5 cut to 0',
+		'48 ease to 0',
+
+		'49.3 cut to 0',
+		'51 ease to 0',
+
+		'54 cut to -10',
+		'66 ease to 10',
+
+		'69.5 cut to 0',
+		'75 ease to 0',
+
+		'75 cut to 0',
+		'90 ease to 0',
+
+		'90 cut to 0',
+		'107 ease to 0',
+	],
+
+	targetcuty: [
+		'17 cut to -750',
+		'19.5 ease to -740',
+		'23 cut to -750',
+		'24 ease to -750',
+		'26.5 ease to -750',
+		'27 cut to -500',
+		'30 ease to -375',
+		'30 cut to -365',
+		'35 ease to -385',
+
+		'42 cut to 0',
+		'44.5 ease to 10',
+		'44.5 cut to -20',
+		'48 linear to 20',
+
+		'49.3 cut to 0',
+		'51 ease to 0',
+
+		'54 cut to 375',
+		'66 ease to 375',
+
+		'69.5 cut to 355',
+		'75 linear to 395',
+		
+		'75 cut to 750',
+		'90 ease to 750',
+
+		'90 cut to 750',
+		'107 ease to 1300',
+	],
+
+	targetcutz: [
+		'24 cut to 100',
+		'26.5 linear to 100',
+		'30 cut to 100',
+		'35 ease to 100',
+
+		'42 cut to 100',
+		'44.5 ease to 100',
+		'44.5 cut to 100',
+		'48 ease to 100',
+
+		'49.3 cut to 100',
+		'51 ease to 10',
+
+		'54 cut to 100',
+		'66 ease to 100',
+
+		'69.5 cut to 100',
+		'75 ease to 100',
+
+		'75 cut to 100',
+		'90 ease to 100',
+
+		'90 cut to 100',
+		'107 ease to 100',
+	],
+
+} );
---
+++ timeline.js
@@ -0,0 +1,157 @@
+var ACTIONS = {
+	CUT: 0,
+	EASE: 1,
+	LINEAR: 2
+}
+
+function Event() {
+	this.start = null;
+	this.end = null;
+	this.action = null;
+	this.from = 0;
+	this.to = 0;
+	this.duration = 0;
+}
+
+function parseStoryline( story ) {
+
+	var result = {};
+
+	for( var v in story ) {
+		if( story.hasOwnProperty( v ) ) {
+
+			var storyboard = [];
+
+			story[ v ].forEach( function( e ) {
+				var start = e.match( /([^\s]+)/ );
+				var event = new Event();
+				if( e.indexOf( 'cut to' ) != -1 ) {
+					event.start = parseFloat( start[ 1 ] );
+					event.end = event.start;
+					event.action = ACTIONS.CUT;
+					var v = e.match( /[^\s]+ cut to ([^\s]+)/ );
+					event.from = parseFloat( v[ 1 ] );
+					event.to = event.from;
+				}
+				if( e.indexOf( 'ease to' ) != -1 ) {
+					event.end = parseFloat( start[ 1 ] );
+					event.action = ACTIONS.EASE;
+					event.from = 0;
+					var v = e.match( /[^\s]+ ease to ([^\s]+)/ );
+					event.to = parseFloat( v[ 1 ] );
+				}
+				if( e.indexOf( 'linear to' ) != -1 ) {
+					event.end = parseFloat( start[ 1 ] );
+					event.action = ACTIONS.LINEAR;
+					event.from = 0;
+					var v = e.match( /[^\s]+ linear to ([^\s]+)/ );
+					event.to = parseFloat( v[ 1 ] );
+				}
+				storyboard.push( event );
+			} );
+
+			storyboard.forEach( function( e, i ) {
+				if( e.action === ACTIONS.EASE || e.action == ACTIONS.LINEAR ) {
+					e.start = storyboard[ i - 1 ].end;
+					e.from = storyboard[ i - 1 ].to;
+				}
+				e.duration = e.end - e.start;
+			} );
+
+			storyboard.forEach( function( e, i ) {
+				//console.log( e.from + '(' + e.start + ')' + ' to ' + e.to + '(' + e.end + ') in ' + e.duration );
+			} );
+
+			result[ v ] = storyboard;
+
+		}
+	}
+
+	return result;
+
+}
+	
+function getPointInStoryline( storyline, t, value ) {
+	
+	if( !storyline[ value ] ) return null;
+
+	for( var j = 0; j < storyline[ value ].length; j++ ) {
+		var e = storyline[ value ][ j ];
+		if( e.start <= t && e.end > t ) {
+			return e;
+		}
+	}
+
+	return null;
+
+}
+
+function averageData( story, t, value ) {
+
+	var p = getPointInStoryline( story, t, value );
+
+	if( !p ) return null;
+
+	if( p.action === ACTIONS.CUT ) {
+		return p.from;
+	}
+
+	if( p.action === ACTIONS.EASE ) {
+	
+		var et = ( t - p.start ) / p.duration;
+		var easing;
+		if ( ( et *= 2 ) < 1 ) easing = 0.5 * et * et;
+		else easing = - 0.5 * ( --et * ( et - 2 ) - 1 );
+
+		var v = p.from + ( easing * ( p.to - p.from ) );
+
+		return v;
+
+	}
+
+	if( p.action === ACTIONS.LINEAR ) {
+	
+		var et = ( t - p.start ) / p.duration;
+		var v = p.from + ( et * ( p.to - p.from ) );
+
+		return v;
+
+	}
+
+}
+
+function setValue( original, value ) {
+
+	if( value !== null ) return value;
+	return original
+
+}
+
+function updateStory( t ) {
+	
+	for( var j = 0; j < flakes.length; j++ ) {
+		flakes[ j ].position.x = averageData( flakeStoryline, t, 'flake' + ( j + 1 ) + 'x' ) || flakes[ j ].position.x;
+		flakes[ j ].position.y = averageData( flakeStoryline, t, 'flake' + ( j + 1 ) + 'y' ) || flakes[ j ].position.y;
+		flakes[ j ].position.z = averageData( flakeStoryline, t, 'flake' + ( j + 1 ) + 'z' ) || flakes[ j ].position.z;
+		flakes[ j ].rotation.z = averageData( flakeStoryline, t, 'flake' + ( j + 1 ) + 'a' ) || flakes[ j ].rotation.z;
+	}
+
+	camera.position.x = setValue( camera.position.x, averageData( cameraStoryline, t, 'x' ) );
+	camera.position.y = setValue( camera.position.y, averageData( cameraStoryline, t, 'y' ) );
+	camera.position.z = setValue( camera.position.z, averageData( cameraStoryline, t, 'z' ) );
+
+	camera.position.x = setValue( camera.position.x, averageData( cameraStoryline, t, 'cutx' ) );
+	camera.position.y = setValue( camera.position.y, averageData( cameraStoryline, t, 'cuty' ) );
+	camera.position.z = setValue( camera.position.z, averageData( cameraStoryline, t, 'cutz' ) );
+
+	camera.target.x = setValue( camera.target.x, averageData( cameraStoryline, t, 'targetx' ) );
+	camera.target.y = setValue( camera.target.y, averageData( cameraStoryline, t, 'targety' ) );
+	camera.target.z = setValue( camera.target.z, averageData( cameraStoryline, t, 'targetz' ) );
+
+	camera.target.x = setValue( camera.target.x, averageData( cameraStoryline, t, 'targetcutx' ) );
+	camera.target.y = setValue( camera.target.y, averageData( cameraStoryline, t, 'targetcuty' ) );
+	camera.target.z = setValue( camera.target.z, averageData( cameraStoryline, t, 'targetcutz' ) );
+
+	fade.style.opacity = setValue( fade.style.opacity, averageData( effectsStoryline, t, 'fade' ) );
+
+}
